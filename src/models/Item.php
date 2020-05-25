12e1d7e3fae9dd3207e95d0186d040230c9b3bc5
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 11:54:19 +0200

    Update Item.php

--- src/models/Item.php
+++ src/models/Item.php
@@ -14,5 +14,9 @@ class Item extends Model{
     protected $table = 'item';
     protected $primaryKey = 'id';
     public $timestamps = false;
+    
+     public function list(){
+    	return $this->belongsTo('\PHPProject\models\list','list_id')->first();
+     }
 }
 ?>
