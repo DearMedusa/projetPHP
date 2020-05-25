c78a6541444e0162c1a57d07450aaedc95dfb0c8
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:05:58 +0200

    Update Item.php

--- src/models/Item.php
+++ src/models/Item.php
@@ -1,1 +1,18 @@
 
+<?php
+
+namespace ProjetPHP\models;
+
+require_once './vendor/autoload.php';
+
+use \Illuminate\Database\Eloquent\Model;
+use \ProjetPhp\models\liste;
+
+
+class Item extends Model{
+
+    protected $table = 'item';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
+?>
