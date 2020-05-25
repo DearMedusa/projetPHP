7478b6e1417429bd61ca40f75b1dd7bc04569ee0
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 11:55:01 +0200

    Update List.php

--- src/models/List.php
+++ src/models/List.php
@@ -12,6 +12,14 @@ class Liste extends Model{
     protected $table = 'liste';
     protected $primaryKey = 'no';
     public $timestamps = false;
+    
+        public function user(){
+    	return $this->belongsTo('\PHPProject\models\List','user_id')->first();
+
+
+    	public function item() {
+        return $this->hasMany('\wishlist\models\Item','liste_id')->get();
+    }
 }
 
 ?>
