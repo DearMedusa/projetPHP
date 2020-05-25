ba4eab33a93c327acbaace4ba831976e65c29cc2
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:06:28 +0200

    Update List.php

--- src/models/List.php
+++ src/models/List.php
@@ -1,1 +1,17 @@
 
+<?php
+
+namespace ProjetPHP\models;
+
+require_once './vendor/autoload.php';
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Liste extends Model{
+
+    protected $table = 'liste';
+    protected $primaryKey = 'no';
+    public $timestamps = false;
+}
+
+?>
