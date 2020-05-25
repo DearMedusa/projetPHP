0b68ac719371a183047e2a17b1dc47e3c0c9f8b3
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 16:43:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,37 @@
-
 <?php
+
+
+require_once 'vendor/autoload.php';
   
   //Imports
-  use listcontroller;
-  use itemcontroller;
-  use usercontroller;
+  use PHPProject\Controller\listController;
+  use PHPProject\Controller\UserController;
+  use PHPProject\Controller\ItemController;
+
 
   // Base de données
   use Illuminate\Database\Capsule\Manager as DB;
   $db = new DB();
-  $db->addConnection(parse_ini_file('src/conf/db.config.ini'));
+  $db->addConnection(parse_ini_file('src/conf/conf.ini'));
   $db->setAsGlobal();
   $db->bootEloquent();
 
   //Eloquent
+  
   session_start();
+
 
   //Slim
   $app = new \Slim\Slim();
 
-  // Affichage de listes
-  $app->get('/', function(){
-    $listController = new ListController();
-    $listController->displayObjects();
+
+
+  //Affichage de listes
+$app->get('/', function(){  //get ou post
+   $uc=new UserController();
+    $uc::EnteteUser(UserController::getUser(0));
   })->name('home');
+  
   
   $app->run();
 ?>
