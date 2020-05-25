b4313780eed6ae50bf72bde78534623f58ba94ec
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:15:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 
+<?php
+  
+  //Imports
+  use listcontroller;
+  use itemcontroller;
+  use usercontroller;
+
+  // Base de données
+  use Illuminate\Database\Capsule\Manager as DB;
+  $db = new DB();
+  $db->addConnection(parse_ini_file('src/conf/db.config.ini'));
+  $db->setAsGlobal();
+  $db->bootEloquent();
+
+  //Eloquent
+  session_start();
+
+  //Slim
+  $app = new \Slim\Slim();
+
+  // Affichage de listes
+  $app->get('/', function(){
+    $listController = new ListController();
+    $listController->displayObjects();
+  })->name('home');
+  
+  $app->run();
+?>
