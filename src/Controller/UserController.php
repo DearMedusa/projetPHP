ac36520ee1f13a0702813009ddc73aff6b03c0ee
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 16:40:33 +0200

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,1 +1,25 @@
+<?php
+  
+namespace PHPProject\Controller;
 
+use \mywishlist\models\Account as Account;
+use \mywishlist\view\AccountView as AccountView;
+
+
+class UserController{
+
+	static function EnteteUser(){
+		$content = "";
+	    UserView::EnteteUser(UserController::getUser());
+	}
+
+
+
+	static function getUser($id){  // voir comment palier session() !!!!!!
+		$user = Account::where('user_id','=', 1)->first();
+		return $user;
+	}
+
+}
+
+?> 
