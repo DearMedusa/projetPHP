5112b7d1d0ea1e2110962656fdd578e9fc8fc6e6
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:07:04 +0200

    Update User.php

--- src/models/User.php
+++ src/models/User.php
@@ -1,1 +1,19 @@
 
+<?php
+
+namespace ProjetPHP\models;
+
+require_once './vendor/autoload.php';
+
+use \Illuminate\Database\Eloquent\Model;
+use \ProjetPhp\models\liste;
+
+
+class User extends Model{
+
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
+
+?>
