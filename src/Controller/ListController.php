6de223db1ba15147af3d5faee3baef0b355dfbc0
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:21:35 +0200

    Update ListController.php

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -1,1 +1,25 @@
 
+<?php
+
+  class ListController{
+    
+    //creation de liste
+    public function createList(){
+    
+    }
+    
+    //edition de liste
+    public function editList(){
+    
+    }
+    
+    //affichage de liste
+    public function afficheList($id){
+      //appel de listView avec en parametre la liste qui correspond à l'id $id
+      $vue = new ListView();
+      $vue->afficherListe(WishList::where('no','=',$id)->where('token','=',$token)->first())
+    }
+  
+  }
+
+?>
