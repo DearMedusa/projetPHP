7386c5236b73dd8bdba890e53c4a462584951724
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-25 11:59:33 +0200

    Update ListView.php

--- src/View/ListView.php
+++ src/View/ListView.php
@@ -1,1 +1,13 @@
+<?php
+  
+  //namespace ?
+  require_once 'vendor/autoload.php';
+  
+  public function afficherListe($liste){
+    //affichage html de la liste
+    foreach ($list->items as $item) {
+      echo("item: ".$item.nom);//placeholder,
+    }
+  }
 
+?>
