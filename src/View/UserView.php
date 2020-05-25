90aa43a2b989ca7b4eb6eb10d07b57176921ec3c
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 16:42:13 +0200

    Update UserView.php

--- src/View/UserView.php
+++ src/View/UserView.php
@@ -1,1 +1,18 @@
+<?php
+  namespace mywishlist\view;
 
+  class UserView{
+  	public static function EnteteUser($user){
+
+      $content = "";
+      $app = \Slim\Slim::getInstance();
+
+      $content = "\n";
+        $content .= "  <!-- Connection form -->\n";
+        $content .= "  <form id='connectionForm' method='post' action='" . $app->urlFor("acc_auth") . "'>\n";
+        $content .= "    <input required placeholder='Login' type='text' name='acc_login'>\n";
+        $content .= "    <input type='submit' value='Connexion'>\n";
+        $content .= "    <a id='inscriptionLink' href='".$app->urlFor('acc_create_get')."'>Inscription</a>\n";
+        $content .= "  </form>\n";
+  	}
+  }
