94aa256a17571c2409350e73dc598a3e7745be51
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-25 16:41:12 +0200

    Update ItemView.php

--- src/View/ItemView.php
+++ src/View/ItemView.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace PHPProject\View;
+use \PHPProject\controller\UserController as UserController;
+
+
+class ItemView{
+
+
+	function affichageItem($item){
+      $app = \Slim\Slim::getInstance();
+      $content = "";
+
+
+      $content .= "\n<!-- Item -->\n";
+      if (isset($item->img)) $content .= "<img src='./img/item/$item->img' class='img-item' alt='Image : $item->nom'>\n";
+      $content .= "<h1> $item->nom </h1>\n";
+      $content .= "<p class='description-item'> $item->descr </p>\n";
+      $content .= "<p> Tarif : $item->tarif € </p>\n";
+      if (isset($item->url)) $content .= "<p> Plus d'infos : <a href='$item->url' target=_blank> $item->url </a></p>\n";
+
+      $content .= "\n<!-- Actions -->\n";
+ 	$this->addContent($content);
+	}
+
+
+}
