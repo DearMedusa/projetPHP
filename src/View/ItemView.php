470e82d49b0ebe0d0a9b9f0b3feec5f71a844a2f
Boubelaye <62295046+Boubelaye@users.noreply.github.com>
2020-05-28 19:34:14 +0200

    Update ItemView.php

--- src/View/ItemView.php
+++ src/View/ItemView.php
@@ -14,13 +14,15 @@ class ItemView{
 	for ($i = 0; $i < $max; $i++) {
             echo("</br>");
             $path="img/".$item[$i]->img;
+            
             if(isset($item[$i]->img)){
                   echo ("<img src='$path' class='img-item' width=\"350\" height=\"250\">\n");
             }else{
                   echo("<i>Image indisponible</i>");
             }
             echo("</br>");
-		echo('<b>Nom :</b> '.$item[$i]->nom."</br>");
+		echo ("<div class='infoItem'>");
+            echo('<b>Nom :</b> '.$item[$i]->nom."</br>");
 		echo('<b>Description</b> : '.$item[$i]->descr."</br>");
             echo('<b>Tarif :</b> '.$item[$i]->tarif."€ </br>");
             echo("<b>Reservé par :</b> ".$item[$i]->reservation);
@@ -29,7 +31,8 @@ class ItemView{
             echo("<form action='$book' method='post'>");
             echo("<input type='submit' value='Reserver'>");
             echo("</form>");
-            echo("<input type='submit' value='Supprimer'></br>");
+            echo("<input type='submit' value='Supprimer'>");
+            echo("</div>");
 		}
       }
 
@@ -38,11 +41,13 @@ class ItemView{
             Outils::headerHTML("Création d'item");
             $slim = \Slim\Slim::getInstance();
             $login = '';
-            echo("<h1>Formulaire d'ajout d'Item</h1>");
+            echo("<h1>Formulaire d'ajout d'item</h1>");
             echo("Veuillez remplir les champs suivants : </br>");
             
             $add = $slim->urlFor('addItem');    //action du bouton (changer l'URL)
-            echo ("<form action='$add' method = 'post'>
+            echo (
+            "<div class='encadrementItem'>
+            <form action='$add' method = 'post'>
             <label for=\"listTitre\">Nom: </label>
             <input type=\"text\" name='item_nom'><br>
             <label for=\"descriptionListe\">Description: </label>
@@ -54,7 +59,8 @@ class ItemView{
             <label for=\"dateExp\">Liste d'appartenance: </label>
             <input type=\"text\" name='liste_id'><br>
             <input type=\"submit\" value=\"Submit\">
-            </form>");
+            </form>
+            </div>");
             Outils::footerHTML();
       }
 
